62891e6a0b88477258585a473dfe7a81c80d7def
PhantomDerp <42085302+PhantomDerp@users.noreply.github.com>
2020-04-06 02:11:33 -0400

    Make constructors into classes
    also added exports and infinite slope protection to getLineFromPoints()

--- library.js
+++ library.js
@@ -7,17 +7,21 @@
 //Made by Louis/Phantomderp
 //Simple library for linear equations and shapes
 //a point is an array with an x and y [x, y]
-//lines cannot be exactly vertical
+//lines cannot be exactly vertical (infinite slope)
 
 //functions
 //returns a line object from 2 points
-function getLineFromPoints(p1, p2) {
+export function getLineFromPoints(p1, p2) {
+  //check for equal y values (infinite slope)
+  if (p1[1] == p2[1]) {
+    p2[1] += 0.00001;
+  }
   var slop = (p1[1] - p2[1]) / (p1[0] - p2[0]);
   return new Line(slop, p1[1] - (p1[0] * slop));
 }
 
 //returns true if the point is in the shape
-function pointInShape(point, shape) {
+export function pointInShape(point, shape) {
   var vals = shape.intercepts(new Line(0, point[1]));
   var val = 0;
   for (var i in vals) {
@@ -27,34 +31,41 @@ function pointInShape(point, shape) {
 }
 
 
-//constructors
+//classes
 
 //Line with slope and y-intercept
-function Line(slope, yint) {
-  this.slope = slope;
-  this.yint = yint;
-  this.eval = function(n) {
+export class Line {
+  constructor(slope, yint) {
+    this.slope = slope;
+    this.yint = yint;
+  }
+  
+  eval(n) {
     return n * this.slope + this.yint;
   };
-  this.intercept = function(line) {
+  
+  intercept(line) {
     var val = (line.yint - this.yint) / (this.slope - line.slope);
     return [val, this.eval(val)];
   };
 }
 
 //shape made of points
-function Shape(points) {
-  this.points = points;
-  if (this.points[0] != this.points[points.length]);
-  this.points.push(this.points[0]);
-  this.lines = [];
+export class Shape {
+  constructor(points) {
+    this.points = points;
+    if (this.points[0] != this.points[points.length - 1]) {
+      this.points.push(this.points[0]);
+    }
+    this.lines = [];
   
-  for (var i = 0; i < this.points.length - 1; i++) {
-    this.lines.push(getLineFromPoints(this.points[i], this.points[i + 1]));
+    for (var i = 0; i < this.points.length - 1; i++) {
+      this.lines.push(getLineFromPoints(this.points[i], this.points[i + 1]));
+    }
   }
   
   //return line intercepts with a line
-  this.lineIntercepts = function(line) {
+  lineIntercepts(line) {
     var returns = [];
     for (var i in this.lines) {
       returns.push(line.intercept(this.lines[i]));
@@ -63,7 +74,7 @@ function Shape(points) {
   };
   
   //return line segment intercepts with a line
-  this.intercepts = function(line) {
+  intercepts(line) {
     var vals = this.lineIntercepts(line);
     var returns = [];
     for (var i = 0; i < vals.length; i++) {
@@ -73,7 +84,7 @@ function Shape(points) {
   };
   
   //return intercepts with another shape
-  this.shapeIntercepts = function(shape) {
+  shapeIntercepts(shape) {
     var list = [];
     var returns = [];
     for (var i = 0; i < shape.lines.length; i++) {
